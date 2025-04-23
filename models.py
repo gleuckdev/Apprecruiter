18467c01b41fb6057e98a065e272f295fde6829d
gleuckdev <gluekdata@gmail.com>
2025-04-23 14:03:08 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,7 @@
+from . import db
+
+class Submission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    resume = db.Column(db.Text, nullable=False)
