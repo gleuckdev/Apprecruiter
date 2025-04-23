1c68142cf1acc03dd63a8a65a770ede45a319fe6
gleuckdev <gluekdata@gmail.com>
2025-04-23 13:30:28 +0530

    Update main.py

--- export/main.py
+++ export/main.py
@@ -7,4 +7,5 @@ app = create_app()
 
 # This file is used as the entry point for the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's dynamic port
+    app.run(host='0.0.0.0', port=port)
