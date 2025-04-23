583b6c1256d85720837cdbb80e83ce3b44915f43
gleuckdev <gluekdata@gmail.com>
2025-04-23 14:20:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-import os
+
+    import os
 
 class Config:
-    # General Configurations
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')  # Replace with a secure key in production
-    DEBUG = os.environ.get('FLASK_DEBUG', 'False') == 'True'
-
-    # Database Configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disables modification tracking to save resources
-    SQLALCHEMY_ECHO = False  # Set to True for verbose SQL logging during development
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
